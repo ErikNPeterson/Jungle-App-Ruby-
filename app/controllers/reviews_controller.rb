6c732a63696e162229c4e8dda94dc275df3e7603
ErikNPeterson <erikeh1@gmail.com>
2018-12-08 18:59:38 -0500

    cool stuff

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,20 @@
 class ReviewsController < ApplicationController
 
-  def create
-    raise "Yay, I'm here!"
-  end
+    def create
+    review = Review.new(
+      product_id: params[:product_id],
+      user_id: current_user.id,
+      description: params[:review][:description],
+      rating: params[:review][:rating]
+    )
+
+    if review.save
+      # send_email(order)
+      redirect_to product_path(:id => params[:product_id]), notice: 'Review Submitted.'
+    else
+      redirect_to product_path(:id => params[:product_id]), notice: 'Sorry, we could not submit your review.'
+    end
+
+  end 
 
 end
