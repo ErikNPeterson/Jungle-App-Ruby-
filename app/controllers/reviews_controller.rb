26c84fe11632740851c81110f958d5bd7cc7fafe
ErikNPeterson <erikeh1@gmail.com>
2018-12-08 20:06:30 -0500

    created a button that renders on each review based on the current_user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,10 @@
 class ReviewsController < ApplicationController
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
     def create
     review = Review.new(
       product_id: params[:product_id],
